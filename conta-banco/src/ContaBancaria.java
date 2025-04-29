bf11e1fc3db52daf1d32519bf1fad469355eb710
Monica <monica.dominog@gmail.com>
2025-04-29 11:34:21 -0300

    informações cliente

---
+++ conta-banco/src/ContaBancaria.java
@@ -0,0 +1,14 @@
+public class ContaBancaria {
+    int numero;
+    int agencia;
+    String titular;
+    double saldo;
+  
+  
+    public  ContaBancaria(int numero, int agencia, String titular, double saldo) {
+      this.numero = numero;
+      this.agencia = agencia;
+      this.titular = titular;
+      this.saldo = saldo;
+    }
+}
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+		System.out.println("Informe o numero da conta bancaria");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Informe o numero da agencia");
+        int numeroAgencia = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Informe o nome do cliente");
+        String nomeTitular = scanner.nextLine();
+
+        System.out.println("Informe o saldo da conta");
+        double saldo = scanner.nextDouble();
+        ContaBancaria conta = new ContaBancaria(numeroConta, numeroAgencia, nomeTitular, saldo);
+
+        System.out.println("Olá " + conta.titular + ", obrigado por criar uma conta em nosso banco,"
+                             + " sua agência é " + conta.agencia + " , conta " + conta.numero +
+                             " e seu saldo " + conta.saldo + " já está disponível para saque.");
+          
+        
     }
 }
